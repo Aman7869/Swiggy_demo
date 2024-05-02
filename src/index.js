f95c7a871aefc60d57e8b44d759e3b6c34ac1e88
[aman] <[amankaithwas57113@gmail.com]>
2024-05-02 09:46:09 +0530

    update code

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename="/Swiggy_demo">
     <Provider store={store}>
       <App />
     </Provider>
